4fa65f85d4dae26cc51e9bcb7aaeb3d7da0cab8e
viniciusog <vinijipa23@gmail.com>
2021-07-12 20:12:00 -0300

    assets

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -3,7 +3,7 @@ import classes from './StartingPageContent.module.css';
 const StartingPageContent = () => {
   return (
     <section className={classes.starting}>
-      <h1>Welcome on Board!</h1>
+      <h2>Welcome on Board!</h2>
     </section>
   );
 };
